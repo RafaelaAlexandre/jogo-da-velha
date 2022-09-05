f204bf6ad94a5655a7ab6976e207ec56cb586846
rafaela-ale-oliveira <103013509+rafaela-ale-oliveira@users.noreply.github.com>
2022-09-05 18:51:28 -0300

    função exibir matriz

--- jogo-da-velha.c
+++ jogo-da-velha.c
@@ -3,13 +3,25 @@ inicio
 */
 #include<stdio.h>
 
-#define LINHA 3
-#define COLUNA 3
+#define LINHA 2
+#define COLUNA 2
+
+void exibirMatriz(char matriz[LINHA][COLUNA]);
 
 int main(){
 	char matriz[LINHA][COLUNA];
 	
-	
+	exibirMatriz(matriz);
+
 	
 	return 0;
+}
+void exibirMatriz(char matriz[LINHA][COLUNA]){
+	int i,j;
+	for(i=0;i<LINHA;i++){
+		for(j=0;j<COLUNA;j++){
+			printf("%c\t|",matriz);
+		}
+		printf("\n");
+	}
 }
