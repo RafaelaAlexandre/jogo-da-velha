f0c49e52b34e092acc1bb244b210f66975c1f6cc
rafaela-ale-oliveira <103013509+rafaela-ale-oliveira@users.noreply.github.com>
2022-09-06 22:37:45 -0300

    exibindo matriz

--- jogo-da-velha.c
+++ jogo-da-velha.c
@@ -17,15 +17,15 @@ int main(){
 	
 	return 0;
 }
-void exibirMatriz(char matriz[LINHA][COLUNA]){
-	int i,j;
-	for(i=0;i<LINHA;i++){
-		for(j=0;j<COLUNA;j++){
-			printf("%c\t|",matriz[i][j]);
-		}
-		printf("\n");
-	}
-}
+//void exibirMatriz(char matriz[LINHA][COLUNA]){
+//	int i,j;
+//	for(i=0;i<LINHA;i++){
+//		for(j=0;j<COLUNA;j++){
+//			printf("%c\t|",matriz[i][j]);
+//		}
+//		printf("\n");
+//	}
+//}
 void criarEspaco(char matriz[LINHA][COLUNA]){
 	int i,j;
 	for(i=0;i<LINHA;i++){
@@ -33,5 +33,22 @@ void criarEspaco(char matriz[LINHA][COLUNA]){
 			matriz[i][j]=' ';	
 		}
 	}
+}
+void exibirMatriz(char matriz[][COLUNA]){
+	int i, j;
+	printf("     0    1    2\n\n");
+	for(i=0;i<LINHA;i++){
+		printf("%d  ",i);
+		for(j=0;j<COLUNA;j++){
+			printf("  %c ", matriz[i][j]);
+			if(j<2){
+				printf("|");
+			}
+		}
+		if(i<2){
+			printf("\n   ---------------\n");
+		}	
+	}
+	printf("\n\n");
 }
 
